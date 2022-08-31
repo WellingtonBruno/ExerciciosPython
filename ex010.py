0c004063f2619314269ffa2d7a99d3056da6ff62
Wellington Bruno <112406632+WellingtonBruno@users.noreply.github.com>
2022-08-30 22:38:50 -0300

    Add files via upload

---
+++ ex010.py
@@ -0,0 +1,5 @@
+#Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Dólares ela pode comprar
+
+n1 = float(input('Digite o valor de sua carteira: '))
+d = n1/3.27
+print("Você pode comprar {:.2f} em Dólares".format(d))
