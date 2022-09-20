87bf1c9b2faca1088dcc1e0c1704fc18c821f593
Wellington Bruno <112406632+WellingtonBruno@users.noreply.github.com>
2022-09-20 12:03:10 -0300

    Update ex026.py

--- ex026.py
+++ ex026.py
@@ -1,5 +1,5 @@
 #Faça um programa que leia uma frase pelo teclado e mostre:
-#Quantas vezes apareace a letra "A".
+#Quantas vezes aparece a letra "A".
 #Em que posição ela aparece a primeira vez
 #Em que posição ela aparece a ultima vez
 
